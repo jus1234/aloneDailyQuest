b9dd349bc4050d685504c1dc743ff4df62aa804d
jus1234 <osjamg1996@gmail.com>
2023-11-16 17:08:50 +0900

    project init

---
+++ aloneDailyQuest/aloneDailyQuest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aloneDailyQuest
+//
+//  Created by 장우석 on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

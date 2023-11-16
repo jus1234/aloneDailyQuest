9a1f21c5579552f4d7182e8ff29fbc526be4b3f8
kimbs5899 <krsder@naver.com>
2023-11-16 17:13:54 +0900

    test commit
    11

---
+++ AloneDailyQuest_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AloneDailyQuest_Project
+//
+//  Created by Designer on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//    print("test")
+}
+

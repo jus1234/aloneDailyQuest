7ece813ae864175a3a0be3a717a5f6e17b699c8e
Matthew(매튜) <krsder@naver.com>
2023-12-09 13:42:31 +0900

    config : 스토리보드 삭제, 시작화면 AccountController로 임시 연결

--- AloneDailyQuest_Project/Controllers/AccountViewController.swift
+++ AloneDailyQuest_Project/Controllers/AccountViewController.swift
@@ -18,10 +18,23 @@ class AccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAddTarget()
+        setupAutoLayout()
     }
     
     func setupAddTarget() {
         
     }
     
+    func configureUI() {
+        loginView.backgroundColor = UIColor(patternImage: UIImage(named: "bottom_background")!)
+    }
+    
+    
+    func setupAutoLayout() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        loginView.leadingAnchor.constraint(equalTo: super.view.leadingAnchor, constant: 0).isActive = true
+        loginView.topAnchor.constraint(equalTo: super.view.topAnchor, constant: 0).isActive = true
+        loginView.trailingAnchor.constraint(equalTo: super.view.trailingAnchor, constant: 0).isActive = true
+        loginView.bottomAnchor.constraint(equalTo: super.view.bottomAnchor, constant: 0).isActive = true
+    }
 }

3141447deab72aaec67c4c2623ec5c4c2de99694
Ben <nonehour@naver.com>
2024-02-16 17:25:14 +0900

    feat: profile 오토레이아웃 설정

--- AloneDailyQuest_Project/Quest_MVC/Views/DetailView.swift
+++ AloneDailyQuest_Project/Quest_MVC/Views/DetailView.swift
@@ -9,6 +9,126 @@ import UIKit
 
 final class DetailView: UIView {
 
+    // MARK: - 프로필 부분
+    lazy var backgroundView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "img_profile_background")
+        return view
+    }()
+    
+    lazy var profileImage: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "img_profile_Lv1-10")
+        return view
+    }()
+    
+    lazy var nickNameTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 0.44, green: 0.22, blue: 0.04, alpha: 1.00)
+        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+        let underlineAttributedString = NSAttributedString(string: "닉네임", attributes: underlineAttribute)
+        label.attributedText = underlineAttributedString
+        return label
+    }()
+    
+    lazy var nickNameText: UILabel = {
+        let label = UILabel()
+        label.text = "매튜"
+        return label
+    }()
+    
+//    lazy var nickNameStackView: UIStackView = {
+//        let stack = UIStackView(arrangedSubviews: [nickNameTitleLabel, nickNameText])
+//        stack.spacing = 10
+//        stack.axis = .horizontal
+//        stack.distribution = .fill
+//        stack.alignment = .fill
+//        stack.translatesAutoresizingMaskIntoConstraints = false
+//        return stack
+//    }()
+    
+    lazy var levelTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 0.44, green: 0.22, blue: 0.04, alpha: 1.00)
+        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+        let underlineAttributedString = NSAttributedString(string: "레벨", attributes: underlineAttribute)
+        label.attributedText = underlineAttributedString
+        return label
+    }()
+    
+    lazy var levelLabel: UILabel = {
+        let label = UILabel()
+        label.text = "LV.1"
+        return label
+    }()
+//
+//    lazy var levelStackView: UIStackView = {
+//        let stack = UIStackView(arrangedSubviews: [levelTitleLabel, levelLabel])
+//        stack.spacing = 20
+//        stack.axis = .horizontal
+//        stack.distribution = .fillEqually
+//        stack.alignment = .fill
+//        stack.translatesAutoresizingMaskIntoConstraints = false
+//        return stack
+//    }()
+    
+    lazy var firstStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [profileImage, nickNameTitleLabel, nickNameText, levelTitleLabel, levelLabel])
+        stack.spacing = 15
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    lazy var experienceTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = UIColor(red: 0.44, green: 0.22, blue: 0.04, alpha: 1.00)
+        let underlineAttribute = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue]
+        let underlineAttributedString = NSAttributedString(string: "경험치", attributes: underlineAttribute)
+        label.attributedText = underlineAttributedString
+        return label
+    }()
+    
+    lazy var experienceBar: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "img_level_bar")
+        let attrString = NSAttributedString(
+            string: "0/10",
+            attributes: [
+                NSAttributedString.Key.strokeColor: UIColor.black,
+                NSAttributedString.Key.foregroundColor: UIColor.white,
+                NSAttributedString.Key.font : UIFont(name: "DungGeunMo", size: 16)
+            ]
+        )
+        view.layer.backgroundColor = UIColor(red: 0.261, green: 0.872, blue: 0.248, alpha: 1).cgColor
+        view.accessibilityAttributedLabel = attrString
+        return view
+    }()
+    
+    lazy var secondStackView: UIStackView = {
+       let stack = UIStackView(arrangedSubviews: [experienceTitleLabel, experienceBar])
+        stack.spacing = 12
+        stack.axis = .horizontal
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    lazy var allStackView: UIStackView = {
+       let stack = UIStackView(arrangedSubviews: [firstStackView, secondStackView])
+        stack.spacing = 16
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    // MARK: - 퀘스트 생성 파트
+
     let basicView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -169,23 +289,66 @@ final class DetailView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor(red: 0.22, green: 0.784, blue: 0.784, alpha: 1)
+        
+        addsubviews()
         setupUI()
+        profileAutoLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setupUI() {
-        self.addSubview(basicView)
+    var firstHeightConstraint: CGFloat = 26
+    var secondHeightConstraint: CGFloat = 18
+    var trailingAnchorConstraint: CGFloat = -4
+    
+    func profileAutoLayout() {
+        self.addSubview(backgroundView)
+        backgroundView.addSubview(allStackView)
+        allStackView.addSubview(firstStackView)
+        allStackView.addSubview(secondStackView)
+        firstStackView.addSubview(profileImage)
+        firstStackView.addSubview(nickNameTitleLabel)
+        firstStackView.addSubview(levelTitleLabel)
+        
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        nickNameTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        levelTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         
         NSLayoutConstraint.activate([
-            // topanchor은 추후에 프로필바 추가 되면 수정 해야함
-            basicView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
-            basicView.bottomAnchor.constraint(equalTo: saveButton.topAnchor, constant: 34),
-            basicView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
-            basicView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -25)
+            backgroundView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 0),
+            backgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            backgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: trailingAnchorConstraint),
+            backgroundView.heightAnchor.constraint(equalToConstant: 100),
+            
+            profileImage.widthAnchor.constraint(equalToConstant: firstHeightConstraint),
+            profileImage.heightAnchor.constraint(equalTo: profileImage.widthAnchor),
+            
+            nickNameTitleLabel.widthAnchor.constraint(equalToConstant: 50),
+            
+            levelLabel.widthAnchor.constraint(equalToConstant: 35),
+            levelTitleLabel.widthAnchor.constraint(equalTo: levelLabel.widthAnchor),
+            
+            
+            
+            firstStackView.heightAnchor.constraint(equalToConstant: firstHeightConstraint),
+            secondStackView.heightAnchor.constraint(equalToConstant: secondHeightConstraint),
+            
+           
+            allStackView.topAnchor.constraint(equalTo: backgroundView.topAnchor, constant: 20),
+            allStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 30),
+            allStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -30)
         ])
+        
+        
+    }
+    
+    func addsubviews() {
+        self.addSubview(basicView)
+        
         self.basicView.addSubview(baseStackView)
         
         self.firstView.addSubview(questLabel)
@@ -193,11 +356,53 @@ final class DetailView: UIView {
         self.thirdView.addSubview(repeatLabel)
         self.fourthView.addSubview(buttonStackView)
         
+        self.addSubview(saveButton)
+    }
+    
+    
+    var stackViewleadingConstraint: CGFloat = 20
+    
+    
+    func setupUI() {
+        
+        
+        
+        // 베이스 뷰 오토레이아웃
         NSLayoutConstraint.activate([
-            questLabel.leadingAnchor.constraint(equalTo: firstView.leadingAnchor, constant: 20),
-            questTextView.leadingAnchor.constraint(equalTo: secondView.leadingAnchor, constant: 20),
-            repeatLabel.leadingAnchor.constraint(equalTo: thirdView.leadingAnchor, constant: 20),
-            buttonStackView.leadingAnchor.constraint(equalTo: fourthView.leadingAnchor, constant: 20)
+            // topanchor은 추후에 프로필바 추가 되면 수정 해야함
+            basicView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+            basicView.bottomAnchor.constraint(equalTo: saveButton.topAnchor, constant: 34),
+            basicView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 25),
+            basicView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -25)
         ])
+        
+        // 각 스택뷰마다 오토레이아웃 설정
+        NSLayoutConstraint.activate([
+            questLabel.leadingAnchor.constraint(equalTo: firstView.leadingAnchor, constant: stackViewleadingConstraint),
+            questLabel.heightAnchor.constraint(equalToConstant: 90),
+            questLabel.widthAnchor.constraint(equalToConstant: 20),
+            questLabel.centerYAnchor.constraint(equalTo: firstView.centerYAnchor),
+            
+            questTextView.topAnchor.constraint(equalTo: secondView.topAnchor, constant: 10),
+            questTextView.bottomAnchor.constraint(equalTo: secondView.bottomAnchor, constant: 10),
+            questTextView.leadingAnchor.constraint(equalTo: secondView.leadingAnchor, constant: stackViewleadingConstraint),
+            questTextView.trailingAnchor.constraint(equalTo: secondView.trailingAnchor, constant: -20),
+            
+            repeatLabel.leadingAnchor.constraint(equalTo: thirdView.leadingAnchor, constant: stackViewleadingConstraint),
+            repeatLabel.heightAnchor.constraint(equalToConstant: 16),
+            repeatLabel.widthAnchor.constraint(equalToConstant: 64),
+            
+            buttonStackView.leadingAnchor.constraint(equalTo: fourthView.leadingAnchor, constant: 24),
+            buttonStackView.trailingAnchor.constraint(equalTo: fourthView.trailingAnchor, constant: -24)
+        ])
+       
+        // 세이브 버튼 오토레이아웃 설정
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: basicView.bottomAnchor, constant: 40),
+            saveButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 38),
+            saveButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -38),
+            saveButton.heightAnchor.constraint(equalToConstant: 64)
+        ])
+        
     }
 }

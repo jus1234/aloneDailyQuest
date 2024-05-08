a8013a3f8a215dbae6d85de68463b38b36ab1877
jus1234 <osjamg1996@gmail.com>
2024-05-08 13:59:18 +0900

    feat: AccountUsecase 생성

---
+++ AloneDailyQuest_Project/Domain/Usecase/AccountUsecase.swift
@@ -0,0 +1,51 @@
+//
+//  AccountUsecase.swift
+//  AloneDailyQuest_Project
+//
+//  Created by Wooseok on 5/8/24.
+//
+
+import Foundation
+
+protocol AccountUsecase {
+    func signup(userId: String) async throws
+    func checkId(userId: String) async throws -> Bool
+    func login(userId: String) async throws -> Bool
+    func fetchUserInfo(userId: String) async throws -> UserInfo
+    func fetchExperience(userId: String) async throws -> Int
+    func addExperience(user: UserInfo) async throws -> Int
+}
+
+final class DefaultAccountUsecase: AccountUsecase {
+    private let repository: AccountRepository
+    
+    init(repository: AccountRepository) {
+        self.repository = repository
+    }
+    
+    func signup(userId: String) async throws {
+        try await repository.signup(userId: userId)
+    }
+    
+    func checkId(userId: String) async throws -> Bool {
+        return try await repository.checkId(userId: userId)
+    }
+    
+    func login(userId: String) async throws -> Bool {
+        return try await repository.checkId(userId: userId)
+    }
+    
+    func fetchUserInfo(userId: String) async throws -> UserInfo {
+        return try await repository.fetchUserInfo(userId: userId)
+    }
+    
+    func fetchExperience(userId: String) async throws -> Int {
+        return try await repository.fetchExperience(userId: userId)
+    }
+    
+    func addExperience(user: UserInfo) async throws -> Int {
+        return try await repository.addExperience(user: user)
+    }
+    
+    
+}

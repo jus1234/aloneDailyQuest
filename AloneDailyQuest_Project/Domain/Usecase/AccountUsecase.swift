8c9a7c3e56cf211eec7e4a6817a5ae82437162aa
jus1234 <osjamg1996@gmail.com>
2024-06-05 17:57:05 +0900

    refactor: AccountUsecase 리팩토링

--- AloneDailyQuest_Project/Domain/Usecase/AccountUsecase.swift
+++ AloneDailyQuest_Project/Domain/Usecase/AccountUsecase.swift
@@ -6,12 +6,11 @@
 //
 
 import Foundation
+import RxSwift
 
 protocol AccountUsecase {
-    func signup(userId: String) async throws
-    func checkId(userId: String) async throws -> Bool
-    func fetchUserInfo(userId: String) async throws -> UserInfo
-    func fetchExperience(userId: String) async throws -> Int
+    func signup(userId: String) -> Single<Bool>
+    func validata(nickName: String) -> Completable
 }
 
 final class DefaultAccountUsecase: AccountUsecase {
@@ -21,19 +20,43 @@ final class DefaultAccountUsecase: AccountUsecase {
         self.repository = repository
     }
     
-    func signup(userId: String) async throws {
-        try await repository.signup(userId: userId)
+    func signup(userId: String) -> Single<Bool> {
+        return Single.create { [weak self] observer in
+            self?.repository
+                .checkId(userId: userId)
+                .subscribe(onSuccess: { result in
+                    guard result else {
+                        observer(.success(false))
+                        return
+                    }
+                    self?.repository
+                        .signup(userId: userId)
+                        .subscribe(onCompleted: {
+                            observer(.success(true))
+                        }, onError: { error in
+                            observer(.failure(error))
+                        })
+                        .disposed(by: DisposeBag())
+                }, onFailure: { error in
+                    observer(.failure(error))
+                })
+                .disposed(by: DisposeBag())
+            return Disposables.create()
+        }
     }
     
-    func checkId(userId: String) async throws -> Bool {
-        return try await repository.checkId(userId: userId)
-    }
-    
-    func fetchUserInfo(userId: String) async throws -> UserInfo {
-        return try await repository.fetchUserInfo(userId: userId)
-    }
-    
-    func fetchExperience(userId: String) async throws -> Int {
-        return try await repository.fetchExperience(userId: userId)
+    func validata(nickName: String) -> Completable {
+        return Completable.create { observer in
+            guard nickName.count > 0 else {
+                observer(.error(NSError()))
+                return Disposables.create()
+            }
+            
+            let nicknameRegex = "^[a-zA-Z0-9가-힣]{1,8}$"
+            let nicknamePredicate = NSPredicate(format: "SELF MATCHES %@", nicknameRegex)
+            nicknamePredicate.evaluate(with: nickName) ? observer(.completed) : observer(.error(NSError()))
+            
+            return Disposables.create()
+        }
     }
 }

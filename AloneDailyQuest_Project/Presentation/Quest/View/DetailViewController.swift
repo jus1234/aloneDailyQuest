706ff6161f900ccf53b77c8be4e41b936ed7ed90
jus1234 <osjamg1996@gmail.com>
2024-06-14 06:27:12 +0900

    refactor: DetailViewController 리팩토링

--- AloneDailyQuest_Project/Presentation/Quest/View/DetailViewController.swift
+++ AloneDailyQuest_Project/Presentation/Quest/View/DetailViewController.swift
@@ -15,13 +15,9 @@ final class DetailViewController: UIViewController {
     
     private let detailView = DetailView()
     private let viewModel: DetailViewModel
-//    private var viewDidLoadEvent: Observable<Void> = Observable(())
-//    private lazy var updateEvent: Observable<QuestInfo?> = Observable(questData)
-//    private lazy var addEvent: Observable<QuestInfo?> = Observable(nil)
-    private lazy var input = DetailViewModel.Input(
-        updateEvent: PublishRelay(),
-        createEvent: PublishRelay(),
-        didBackButtonTapEvent: detailView.backButton.rx.tap)
+    
+    private var questData: QuestInfo?
+    private var isDaySelected = [false, false, false, false, false, false, false]
     
     init(viewModel: DetailViewModel, questData: QuestInfo?) {
         self.viewModel = viewModel
@@ -33,36 +29,86 @@ final class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    var questData: QuestInfo?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view = detailView
         setup()
         configureUI()
-        bindViewModel()
+        bind()
     }
     
-    func bindViewModel() {
+    func bind() {
+        let input = DetailViewModel.Input(
+            saveQuest: detailView.saveButton.rx.tap
+                .subscribe(on: MainScheduler())
+                .map { [weak self] _ -> Bool in
+                    guard
+                        let content = self?.detailView.questTextView.text,
+                        let selectedDays = self?.isDaySelected
+                    else {
+                        return false
+                    }
+                    if content == "" || content == "퀘스트를 입력하세요." {
+                        self?.completedAlert(message: "퀘스트 내용을 입력해 주세요.")
+                        return false
+                    }
+                    if selectedDays.filter({ $0 }).isEmpty {
+                        self?.completedAlert(message: "하나 이상의 요일을 선택해 주세요.")
+                        return false
+                    }
+                    return true
+                }
+                .map { [weak self] isValid in
+                    guard isValid else {
+                        return nil
+                    }
+                    guard
+                        let quest = self?.makeQuest()
+                    else {
+                        return nil
+                    }
+                    return quest
+                }
+            ,
+            didBackButtonTapEvent: detailView.backButton.rx.tap)
         let output = viewModel.transform(input: input)
         
-        output.errorMessage
-            .asDriver(onErrorJustReturn: "")
-            .drive(with: self) { owner, error in
-                owner.completedAlert(message: error)
-            }
+        output.saveResult
+            .asDriver(onErrorJustReturn: false)
+            .drive(with: self)
             .disposed(by: disposeBag)
-    }
-    
-    func setup() {
-        detailView.questTextView.delegate = self
-        detailView.buttons.forEach{ button in
-            button.addTarget(self, action: #selector(dayButtonTapped), for: .touchUpInside)
+        
+        output.viewChanged
+            .asDriver(onErrorJustReturn: ())
+            .drive(with: self)
+            .disposed(by: disposeBag)
+        
+        zip(detailView.buttons, 0...6).forEach { [weak self] button, index in
+            guard 
+                let disposebag = self?.disposeBag
+            else {
+                return
+            }
+            button.rx.tap
+                .subscribe(on: MainScheduler())
+                .subscribe { _ in
+                    self?.isDaySelected[index].toggle()
+                    guard 
+                        let isSelected = self?.isDaySelected[index]
+                    else {
+                        return
+                    }
+                    self?.toggleButtonAppearance(button: button, isSelected: isSelected)
+                }
+                .disposed(by: disposebag)
         }
-        detailView.saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
     }
     
-    func configureUI() {
+    private func setup() {
+        detailView.questTextView.delegate = self
+    }
+    
+    private func configureUI() {
         if let questData = self.questData {
             detailView.titleText.text = "퀘스트수정"
             detailView.titleBackgroundText.text = "퀘스트수정"
@@ -71,48 +117,28 @@ final class DetailViewController: UIViewController {
             detailView.questTextView.textColor = .black
             detailView.questTextView.becomeFirstResponder()
             isDaySelected = questData.selectedDate
-            zip(detailView.buttons, questData.selectedDate).forEach { toggleButtonAppearance(button: $0.0, isSelected: $0.1) }
+            zip(detailView.buttons, questData.selectedDate)
+                .forEach { toggleButtonAppearance(button: $0.0, isSelected: $0.1) }
         } else {
             detailView.titleText.text = "퀘스트생성"
             detailView.titleBackgroundText.text = "퀘스트생성"
             
             detailView.questTextView.text = "퀘스트를 입력하세요."
             detailView.questTextView.textColor = .lightGray
-            
         }
     }
     
-    var isDaySelected = [false, false, false, false, false, false, false]
-    var day = ["월", "화", "수", "목", "금", "토", "일"]
-    var repeatLabel = ""
-    var isCompleted = false
-    var todayExp: Int64 = 0
-    var questImage = "img_quest_ing"
-    var expString = "보상 : 20xp"
-    
-    func repeatLabelSet() -> String {
-        for (index, bool) in isDaySelected.enumerated() {
-            if bool {
-                repeatLabel = repeatLabel + day[index]
-            }
-        }
-        
-        if repeatLabel == "" {
-            return "반복 없음"
-        } else if repeatLabel == "월화수목금토일" {
-            return "매일 반복"
-        } else {
-            return "요일 반복"
-        }
-    }
-
-    @objc func dayButtonTapped(_ sender: UIButton){
-        guard let index = detailView.buttons.firstIndex(of: sender) else { return }
-        isDaySelected[index].toggle()
-        toggleButtonAppearance(button: sender, isSelected: isDaySelected[index])
+    private func makeQuest() -> QuestInfo {
+        return QuestInfo(
+            id: questData?.id ?? UUID(),
+            quest: detailView.questTextView.text ?? "",
+            date: questData?.date ?? Date(),
+            selectedDate: isDaySelected,
+            repeatDay: isDaySelected.filter({ $0 }).count == 7 ? "매일 반복" : "요일 반복",
+            completed: questData?.completed ?? false)
     }
     
-    func toggleButtonAppearance(button: UIButton, isSelected: Bool) {
+    private func toggleButtonAppearance(button: UIButton, isSelected: Bool) {
         if isSelected {
             button.backgroundColor = UIColor(red: 0.63, green: 0.28, blue: 0.01, alpha: 1.00)
             button.setTitleColor(.white, for: .normal)
@@ -120,39 +146,6 @@ final class DetailViewController: UIViewController {
             button.backgroundColor = UIColor(red: 1.00, green: 0.90, blue: 0.78, alpha: 1.00)
             button.setTitleColor(.black, for: .normal)
         }
-    }
-    
-    @objc func saveButtonTapped(_ sender: UIButton) {
-        if var questData = self.questData {
-            let repeatLabel = repeatLabelSet()
-            questData.quest = detailView.questTextView.text
-            questData.selectedDate[0] = isDaySelected[0]
-            questData.selectedDate[1] = isDaySelected[1]
-            questData.selectedDate[2] = isDaySelected[2]
-            questData.selectedDate[3] = isDaySelected[3]
-            questData.selectedDate[4] = isDaySelected[4]
-            questData.selectedDate[5] = isDaySelected[5]
-            questData.selectedDate[6] = isDaySelected[6]
-            questData.repeatDay = repeatLabel
-            input.updateEvent.accept(questData)
-            
-            self.questData = questData
-            
-            // 기존데이터가 없을때 ===> 새로운 데이터 생성
-        } else {
-            guard let questText = detailView.questTextView.text else { return }
-            let repeatLabel = repeatLabelSet()
-            registerQuestForSelectedDays(questText: questText, repeatLabel: repeatLabel)
-        }
-    }
-    
-    func registerQuestForSelectedDays(questText: String, repeatLabel: String) {
-        let data = QuestInfo(id: UUID(), quest: questText, date: Date(), selectedDate: isDaySelected, repeatDay: repeatLabel, completed: isCompleted)
-        input.createEvent.accept(data)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
     }
 }
 

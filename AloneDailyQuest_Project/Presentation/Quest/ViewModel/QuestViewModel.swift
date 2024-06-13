cf38cf1f850018a8459c31fbcfeeef2b04725dc6
jus1234 <osjamg1996@gmail.com>
2024-06-14 06:26:22 +0900

    refactor: QuestViewModel 리팩토링

--- AloneDailyQuest_Project/Presentation/Quest/ViewModel/QuestViewModel.swift
+++ AloneDailyQuest_Project/Presentation/Quest/ViewModel/QuestViewModel.swift
@@ -2,7 +2,7 @@
 //  QuestViewModel.swift
 //  AloneDailyQuest_Project
 //
-//  Created by Matthew on 5/10/24.
+//  Created by wooseok on 6/13/24.
 //
 
 import Foundation
@@ -11,157 +11,108 @@ import RxSwift
 import RxCocoa
 
 final class QuestViewModel: ViewModel {
-    private var disposeBag = DisposeBag()
-    
     struct Input {
         var viewWillAppear: ControlEvent<Bool>
-        var deleteQuestEvent: PublishRelay<QuestInfo?>
-        var qeusetViewEvent: ControlEvent<Void>
-        var rankViewEvent: ControlEvent<Void>
-        var profileViewEvent: ControlEvent<Void>
-        var didPlusButtonTap: ControlEvent<Void>
-        var updateQuestEvent: PublishRelay<QuestInfo>
-        var completeQuestEvent: PublishRelay<QuestInfo>
+        var loadQuests: PublishRelay<Void>
+        var deleteQuest: PublishRelay<QuestInfo>
+        var moveToRankView: ControlEvent<Void>
+        var moveToProfileView: ControlEvent<Void>
+        var createQuest: ControlEvent<Void>
+        var updateQuest: PublishRelay<QuestInfo>
+        var completeQuest: PublishRelay<QuestInfo>
+        var dayChanged: Observable<Notification>
     }
     
     struct Output {
-        var questList: PublishRelay<[QuestInfo]>
-        var errorMessage: PublishRelay<String>
-        var userInfo: PublishRelay<UserInfo?>
+        var quests: Observable<[QuestInfo]>
+        var updateResult: Observable<Bool>
+        var delereResult: Observable<Void>
+        var viewChanged: Observable<Void>
+        var resetDailyQuests: Observable<Void>
     }
     
     private let usecase: QuestUsecase
     private let coordinator: QuestCoordinator
     
-    private let output = Output(
-        questList: PublishRelay(),
-        errorMessage: PublishRelay(),
-        userInfo: PublishRelay())
-    
     init(usecase: QuestUsecase, coordinator: QuestCoordinator) {
         self.usecase = usecase
         self.coordinator = coordinator
-        scheduleMidnightUpdate()
     }
     
     func transform(input: Input) -> Output {
-        input.viewWillAppear
-            .subscribe(onNext: { [weak self] _ in
-                self?.updateDailyQuest()
-            })
-            .disposed(by: disposeBag)
+        let viewWillAppear = input.viewWillAppear
+            .map { _ in}
+            .asObservable()
         
-        input.deleteQuestEvent
-            .subscribe(onNext: { [weak self] quest in
-                guard let quest else {
-                    return
+        let dayChanged = input.dayChanged
+            .map { _ in}
+            .asObservable()
+        
+        let resetDailyQuests = Observable.merge([viewWillAppear, dayChanged])
+            .flatMap { [weak self] _ -> Single<Void> in
+                guard let resetDailyQuests = self?.usecase.resetDailyQuests() else {
+                    return .error(NSError())
                 }
-                self?.delete(quest: quest)
-                self?.fetchQuests()
-            })
-            .disposed(by: disposeBag)
+                return resetDailyQuests
+            }
+            .asObservable()
         
-        input.updateQuestEvent
-            .subscribe(onNext: { [weak self] quest in
-                self?.coordinator.connectDetailCoordinator(quest: quest)
-            })
-            .disposed(by: disposeBag)
-        
-        input.completeQuestEvent
-            .subscribe(onNext: { [weak self] quest in
-                guard let self, let nickName = UserDefaults.standard.string(forKey: "nickName") else {
-                    return
+        let quests = input.loadQuests
+            .flatMap { [weak self] _ -> Observable<[QuestInfo]> in
+                guard let quests = self?.usecase.fetchQuests().asObservable() else {
+                    return .empty()
                 }
-                usecase.isTodayExperienceAcquisitionMax()
-                    .subscribe(onSuccess: {
-                        self.update(quest: quest)
-                        self.addExperience(nickName: nickName)
-                        self.fetchQuests()
-                    }, onFailure: {_ in 
-                        self.output.errorMessage.accept("일일 최대 획득 경험치를 초과했습니다.")
-                    })
-                    .disposed(by: disposeBag)
-            })
-            .disposed(by: disposeBag)
+                return quests
+            }
         
-        input.rankViewEvent
-            .subscribe(onNext: { [weak self] _ in
-                self?.coordinator.finish(to: .ranking)
-            })
-            .disposed(by: disposeBag)
+        let updateResult = input.completeQuest
+            .flatMap { [weak self] quest -> Single<Bool> in
+                guard let result = self?.usecase.complete(quest: quest) else {
+                    return .error(NSError())
+                }
+                return result
+            }
+            .asObservable()
         
-        input.profileViewEvent
-            .subscribe(onNext: { [weak self] _ in
-                self?.coordinator.finish(to: .profile)
-            })
-            .disposed(by: disposeBag)
+        let deleteResult = input.deleteQuest
+            .flatMap { [weak self] quest -> Single<Bool> in
+                guard let result = self?.usecase.delete(quest: quest) else {
+                    return .error(NSError())
+                }
+                return result
+            }
+            .map { _ in return }
+            .asObservable()
         
-        input.didPlusButtonTap
-            .subscribe(onNext: { [weak self] _ in
+        let toDetailToCreate = input.createQuest
+            .do(onNext: { [weak self] _ in
                 self?.coordinator.connectDetailCoordinator(quest: nil)
             })
-            .disposed(by: disposeBag)
         
-        return output
-    }
-    
-    private func fetchQuests() {
-        usecase.fetchQuests()
-            .subscribe(onSuccess: { [weak self] quests in
-                self?.output.questList.accept(quests)
+        let toDetailToUpdate = input.updateQuest
+            .do(onNext: { [weak self] quest in
+                self?.coordinator.connectDetailCoordinator(quest: quest)
             })
-            .disposed(by: disposeBag)
-    }
-    
-    private func update(quest: QuestInfo) {
-        usecase.update(quest: quest)
-            .subscribe( onError: { [weak self] error in
-                self?.output.errorMessage.accept(error.localizedDescription)
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    private func addExperience(nickName: String) {
-        usecase.addExperience(userId: nickName, experience: 1)
-            .subscribe(onSuccess: { [weak self] experience in
-                UserDefaults.standard.set(experience, forKey: "experience")
-                self?.output.userInfo.accept(UserInfo(nickName: nickName, experience: experience))
-            }, onFailure: { [weak self] error in
-                self?.output.errorMessage.accept(error.localizedDescription)
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    private func delete(quest: QuestInfo) {
-        usecase.delete(quest: quest)
-            .subscribe(onError: { [weak self] _ in
-                self?.output.errorMessage.accept("퀘스트 삭제를 실패했습니다.")
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    private func updateDailyQuest() {
-        Single.zip(usecase.resetDailyQuests(), usecase.fetchQuests())
-            .subscribe(onSuccess: { [weak self] _, quests in
-                self?.output.questList.accept(quests)
-            }, onFailure: { [weak self] _ in
-                self?.output.errorMessage.accept("처리 중 문제가 발생했습니다.")
-            })
-            .disposed(by: disposeBag)
-    }
-}
-
-extension QuestViewModel {
-    private func scheduleMidnightUpdate() {
-        let now = Date()
-        let calendar = Calendar.current
-        guard let midnight = calendar.nextDate(after: now, matching: DateComponents(hour: 0, minute: 0, second: 0), matchingPolicy: .nextTime) else {
-            return
-        }
-        let timeInterval = midnight.timeIntervalSince(now)
-        DispatchQueue.main.asyncAfter(deadline: .now() + timeInterval) {
-            self.updateDailyQuest()
-        }
+            .map { _ in }
         
+        let toRanking = input.moveToRankView
+            .do(onNext: { [weak self] _ in
+                self?.coordinator.finish(to: .ranking)
+            })
+        
+        let toProfile = input.moveToProfileView
+            .do(onNext: {[weak self] _ in
+                self?.coordinator.finish(to: .profile)
+            })
+        
+        let viewChanged = Observable.merge(
+            [toDetailToCreate, toDetailToUpdate, toRanking, toProfile])
+        
+        return Output(
+            quests: quests,
+            updateResult: updateResult,
+            delereResult: deleteResult,
+            viewChanged: viewChanged,
+            resetDailyQuests: resetDailyQuests)
     }
 }
